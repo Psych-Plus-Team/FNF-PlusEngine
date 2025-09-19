a03924534ec4bdb59b23a4fd43da533b2f86b2b4
Lenin <medinastoleninliu@gmail.com>
2025-09-19 15:44:32 -0500

    changed funkin-modchart to git version

--- setup/unix.sh
+++ setup/unix.sh
@@ -10,8 +10,7 @@ echo This might take a few moments depending on your internet speed.
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-ui 2.6.2
-haxelib install funkin-modchart 1.2.3
-haxelib install sl-windows-api 1.1.0
+    haxelib install sl-windows-api 1.1.0
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
@@ -20,6 +19,7 @@ haxelib install hxvlc 2.0.1
 haxelib install lime 8.1.2
 haxelib run lime setup
 haxelib install openfl 9.3.3
+haxelib git funkin-modchart https://github.com/LeninAsto/FunkinModchart.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
