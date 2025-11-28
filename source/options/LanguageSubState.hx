0a275e2648da93db3a144dc5beb0d48dad612156
MTGC <satriyoadi130@gmail.com>
2025-11-28 17:09:07 +0700

    Update LanguageSubState.hx

--- source/options/LanguageSubState.hx
+++ source/options/LanguageSubState.hx
@@ -222,7 +222,7 @@ class LanguageSubState extends MusicBeatSubstate
 			'nl-NL' => 'Dit is een voorbeeldtekst in de Nederlandse taal Nederland',
 			'zh-CN' => '这是中文（简体）语言的示例文本',
 			'zh-HK' => '這是中文（香港）語言的示例文本',
-			"id-ID" => 'Ini adalah teks contoh dalam bahasa Indonesia Indonesia'
+			"id-ID" => 'Ini adalah teks contoh dalam bahasa Indonesia Nusantara'
 		];
 		
 		// Buscar por código de idioma exacto primero
