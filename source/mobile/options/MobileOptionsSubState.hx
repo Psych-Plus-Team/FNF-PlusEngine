914ac111c38d9f742aebb76b3cbd995a11e872c6
MTGC <satriyoadi130@gmail.com>
2025-12-06 13:39:56 +0700

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -35,7 +35,7 @@ class MobileOptionsSubState extends BaseOptionsMenu
 
 	public function new()
 	{
-		title = 'Mobile Options';
+		title = Language.getPhrase('options_mobile_menu', 'Mobile Options');
 		rpcTitle = 'Mobile Options Menu'; // for Discord Rich Presence, fuck it
 
 		option = new Option('Extra Controls', 'Select how many extra buttons you prefer to have?\nThey can be used for mechanics with LUA or HScript.',
