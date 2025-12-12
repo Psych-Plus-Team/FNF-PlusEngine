8534eb749bfb46f912d7d6a872e437087c529157
MTGC <satriyoadi130@gmail.com>
2025-12-12 14:39:22 +0700

    i don't know

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -4,7 +4,7 @@ import flixel.graphics.FlxGraphic;
 
 import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import flixel.util.FlxDestroyUtil;
-import flixel.addons.FlxAnimate;
+import flixel.flxanimate.PsychFlxAnimate;
 
 import openfl.net.FileReference;
 import openfl.events.Event;
