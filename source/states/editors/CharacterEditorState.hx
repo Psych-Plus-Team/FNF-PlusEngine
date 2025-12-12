10379efad3b89b24e45a9e980e5bf1b01064995d
MTGC <satriyoadi130@gmail.com>
2025-12-12 14:44:27 +0700

    I tried it

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -4,7 +4,7 @@ import flixel.graphics.FlxGraphic;
 
 import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import flixel.util.FlxDestroyUtil;
-import flixel.flxanimate.PsychFlxAnimate;
+import flixel.PsychFlxAnimate;
 
 import openfl.net.FileReference;
 import openfl.events.Event;
